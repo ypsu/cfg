f5999b0b2be4ccf2cb974178d31f8b95fbc71b91
Balazs Kezes <3j4b0y1i@anonaddy.me>
2023-02-11 06:52:18 +0100

    tlog: rewrite it into a much simpler form

--- utils/tlog.go
+++ utils/tlog.go
@@ -7,100 +7,77 @@ import (
 	"os"
 	"os/exec"
 	"path"
-	"syscall"
+	"strings"
 	"time"
 )
 
 func usage() {
 	out := flag.CommandLine.Output()
 	fmt.Fprintln(out, "tlog: timestamped logging.")
-	fmt.Fprintln(out, "starts vim which constantly autosaves its buffer to a logfile along with a timestamp.")
-	fmt.Fprintln(out, "it nags minutes for an update after 20 minutes.")
+	fmt.Fprintln(out, "every 20 minutes starts alerting to enter an update.")
+	fmt.Fprintln(out, "just press enter to start vim, save the update, and quit.")
+	fmt.Fprintln(out, "")
 	fmt.Fprintln(out, "flags:")
 	flag.PrintDefaults()
 }
 
 var logfileFlag = flag.String("l", path.Join(os.Getenv("HOME"), ".tlog"), "logfile to append to.")
-var notefile = "/tmp/.sysstatmsg"
+var notefile = "/tmp/.tnote"
+
+// notifier continuously alerts whenever the last edit on the note file seems too old.
+func notifier() {
+	const target = 20 * time.Minute
+	for {
+		time.Sleep(target)
+		for {
+			s, err := os.Stat(notefile)
+			if err == nil && time.Now().Sub(s.ModTime()) < target {
+				break
+			}
+			os.Stdout.Write([]byte{7})
+			time.Sleep(1 * time.Minute)
+		}
+	}
+}
 
 func main() {
 	flag.Usage = usage
 	flag.Parse()
-
-	noteFile, err := os.Create(notefile)
-	if err != nil {
-		log.Fatal(err)
-	}
-	noteFile.Close()
-
-	ifd, err := syscall.InotifyInit()
-	if err != nil {
-		log.Fatal(err)
-	}
-	_, err = syscall.InotifyAddWatch(ifd, notefile, syscall.IN_CLOSE_WRITE)
-	if err != nil {
-		log.Fatal(err)
-	}
 
 	logfile, err := os.OpenFile(*logfileFlag, os.O_APPEND|os.O_WRONLY, 0644)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	notechan := make(chan string)
+	go notifier()
 
-	go func() {
-		buf := [256]byte{}
-		for {
-			if _, err := syscall.Read(ifd, buf[:]); err != nil {
-				log.Fatal(err)
-			}
-			notebytes, err := os.ReadFile(notefile)
-			if err != nil {
-				log.Fatal(err)
-			}
-			notechan <- string(notebytes)
-		}
-	}()
+	buf := make([]byte, 8)
+	for {
+		// wait for enter.
+		os.Stdin.Read(buf)
+		os.Stdout.WriteString("\033[A") // move the cursor back up.
 
-	go func() {
-		cmd := exec.Command("vim", "-c", "autocmd TextChanged * silent write", notefile)
+		// run vim.
+		os.Remove(notefile)
+		cmd := exec.Command("vim", notefile)
 		cmd.Stdin, cmd.Stdout, cmd.Stderr = os.Stdin, os.Stdout, os.Stderr
-		if err = cmd.Run(); err != nil {
+		if err := cmd.Run(); err != nil {
 			log.Fatalf("vim failed: %v", err)
 		}
-		notechan <- "\000"
-	}()
 
-	done := false
-	timer := time.NewTimer(time.Hour)
-	for !done {
-		var note string
-
-		// wait until a note appears but nag for an update after 20 minutes.
-		timer.Reset(20 * time.Minute)
-		select {
-		case note = <-notechan:
-		case <-timer.C:
-		loop:
-			for {
-				os.Stdout.Write([]byte{7}) // sound the bell.
-				timer.Reset(time.Minute)
-				select {
-				case note = <-notechan:
-					break loop
-				case <-timer.C:
-				}
-			}
+		// read note.
+		notebytes, err := os.ReadFile(notefile)
+		if err != nil {
+			log.Fatal(err)
 		}
-		timer.Stop()
-
-		if note == "\000" {
-			done = true
-			note = ""
+		if len(notebytes) == 0 {
+			continue
 		}
+		note := strings.TrimSpace(string(notebytes))
+
+		// log note.
 		t := time.Now().UTC().Format("2006-01-02.15:04:05")
 		fmt.Fprintf(logfile, "%s %q\n", t, note)
+		fmt.Printf("%s %s\n", t, note)
 	}
-	os.Remove(notefile)
 }
